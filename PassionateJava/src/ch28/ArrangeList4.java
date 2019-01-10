3466613d12d6f8aab359a06dfd3953e01992be5b
zxcv5500 <zxcv5500@121.169.154.250>
2019-01-10 21:41:37 +0900

    같은 이름 class 에러 발생 회피를 위해 class 이름 변경함.

--- PassionateJava/src/ch28/ArrangeList4.java
+++ PassionateJava/src/ch28/ArrangeList4.java
@@ -1,12 +1,12 @@
 package ch28;
 
-import java.util.List;
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 
-class JustSort {
+class JustSort4 {
     public void sort(List<?> lst) {    // 인스턴스 메소드
         Collections.reverse(lst);
     }
@@ -16,7 +16,7 @@ class ArrangeList4 {
     public static void main(String[] args) {
         List<Integer> ls = Arrays.asList(1, 3, 5, 7, 9);
         ls = new ArrayList<>(ls);
-        JustSort js = new JustSort();
+        JustSort4 js = new JustSort4();
 
         Consumer<List<Integer>> c = js::sort;
         c.accept(ls);
