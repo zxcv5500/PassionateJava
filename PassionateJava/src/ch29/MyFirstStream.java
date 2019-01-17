5cd3caf54704968c793a053917f4c186aa1d042e
pangyo255 <pangyo255@gmail.com>
2019-01-17 13:51:13 +0900

    첫 번째 스트림 설명 추가 작성함.

--- PassionateJava/src/ch29/MyFirstStream.java
+++ PassionateJava/src/ch29/MyFirstStream.java
@@ -8,15 +8,12 @@ public class MyFirstStream {
 	public static void main(String[] args) {
 		int[] ar = {1, 2, 3, 4, 5};
 		
-		// 스트림 생성
-		IntStream stm1 = Arrays.stream(ar);
+		IntStream stm1 = Arrays.stream(ar); // 배열 ar로 부터 스트림 생성
 		
-		// 중간 파이프 구성
-		IntStream stm2 = stm1.filter(n-> n%2 == 1);
-		
-		// 최종 파이프 구성
-		int sum = stm2.sum();
-		
+		IntStream stm2 = stm1.filter(n-> n%2 == 1);		// 중간 파이프 연산 진행
+
+		int sum = stm2.sum();			// 최종 파이프 연산 진행 		
+										// 최종 연산을 생략하면 아무 결과도 얻지 못한다. 
 		System.out.println(sum);
 	}
 
