9797649c8de6935dbe5cf1f493bbf45f6bd9c9bb
pangyo255 <pangyo255@gmail.com>
2019-01-17 11:22:15 +0900

    static 메소드의 참조 방식( l인자를 전달 받아서 l을 소모하는 형태라면 메소드 참조 방식을 사용해서 표시할 수 있다)

--- PassionateJava/src/ch28/ArrangeList2.java
+++ PassionateJava/src/ch28/ArrangeList2.java
@@ -1,11 +1,16 @@
 package ch28;
 
-import java.util.List;
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 
+/**
+ * @author Administrator
+ * static 메소드의 참조
+ * l인자를 받아서 같은 l인자를 소모하는 경우 메소드 참조 방식으로 표시할 수 있다.
+ */
 class ArrangeList2 {
     public static void main(String[] args) {
         List<Integer> ls = Arrays.asList(1, 3, 5, 7, 9);
