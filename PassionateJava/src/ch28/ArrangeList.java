3a9b7934ae28abd134edeba3e57aa6790c457600
pangyo255 <pangyo255@gmail.com>
2019-01-17 11:15:37 +0900

    메서드 참조 가능 상황 주석 설명 추가함.

--- PassionateJava/src/ch28/ArrangeList.java
+++ PassionateJava/src/ch28/ArrangeList.java
@@ -1,9 +1,9 @@
 package ch28;
 
-import java.util.List;
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 
 class ArrangeList {
@@ -11,7 +11,8 @@ class ArrangeList {
         List<Integer> ls = Arrays.asList(1, 3, 5, 7, 9);
         ls = new ArrayList<>(ls);
 
-        Consumer<List<Integer>> c = l -> Collections.reverse(l);
+        Consumer<List<Integer>> c = l -> Collections.reverse(l);		// l 인자를 던져서 l을 소모하는 람다식 형태인 경우
+        																// 메서드 참조 형식으로 사용할 수 있다
         c.accept(ls);
         System.out.println(ls);
     }
