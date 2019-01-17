1a2961484f93f76828aa7b025af09df09588cb80
pangyo255 <pangyo255@gmail.com>
2019-01-17 14:19:24 +0900

    filter 중간 스트림에 대한 예제 코드 작성.

---
+++ PassionateJava/src/ch29/FilterStream.java
@@ -0,0 +1,21 @@
+package ch29;
+
+import java.util.Arrays;
+import java.util.List;
+
+class FilterStream {
+
+	public static void main(String[] args) {
+		int[] ar = {1, 2, 3, 4, 5};
+		Arrays.stream(ar)
+				.filter(n->n%2 == 1)
+				.forEach(n->System.out.print(n + "\t"));
+		System.out.println();
+		
+		List<String> sl = Arrays.asList("Toy", "Robot", "Box");
+		sl.stream()
+			.filter(s->s.length() == 3)
+			.forEach(s->System.out.print(s + "\t"));
+		System.out.println();
+	}
+}
